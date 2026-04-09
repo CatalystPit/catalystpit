268b52b4dc61b6686069dd62542e7212e3200b03
CatalystPit <info@catalystpit.com>
2026-04-09 16:10:36 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 'use client'
 
-import CatalystPit from '../components/CatalystPit'
+import dynamic from 'next/dynamic'
+
+const CatalystPit = dynamic(
+  () => import('../components/CatalystPit'),
+  { ssr: false }
+)
 
 export default function Home() {
   return <CatalystPit />
